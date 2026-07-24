9317bc9cbeab63cc79a1f7e4f532cfebae8a2840
AlanPipko <alan.pipko@gmail.com>
2026-07-23 23:17:01 -0400

    Swap resume to AlanPipko_Resume.pdf

--- lib/data/links.ts
+++ lib/data/links.ts
@@ -2,5 +2,5 @@ export const links = {
   github: "https://github.com/AlanP70",
   linkedin: "https://www.linkedin.com/in/alan-pipko-944828342/",
   email: "apipko@uoguelph.ca",
-  resume: "/Main_Resume.pdf",
+  resume: "/AlanPipko_Resume.pdf",
 };
